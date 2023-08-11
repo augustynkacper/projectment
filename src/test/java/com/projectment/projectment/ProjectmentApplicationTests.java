44063a8d88964e0efebe863baf1677c30278a906
augustynkacper <kacperaugustyn@o2.pl>
2023-08-11 22:26:18 +0200

    initizr

---
+++ src/test/java/com/projectment/projectment/ProjectmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectment.projectment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
